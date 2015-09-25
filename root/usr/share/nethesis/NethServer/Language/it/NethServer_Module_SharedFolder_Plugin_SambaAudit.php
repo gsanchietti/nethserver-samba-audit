73e18f710348cb3e3768fbd67034f6bedba8193a
Giacomo Sanchietti <giacomo.sanchietti@nethesis.it>
2015-09-25 10:42:12 +0200

    Remove Italian translation. Refs #3265

--- root/usr/share/nethesis/NethServer/Language/it/NethServer_Module_SharedFolder_Plugin_SambaAudit.php
+++
@@ -1,8 +0,0 @@
-<?php 
-
-/* NethServer_Module_SharedFolder_Plugin_Samba.php translation, language: it */
-
-$L['SmbAuditStatus_label'] = 'Abilita auditing sulla cartella condivisa';
-$L['SambaAudit_Title'] = 'Samba Audit';
-$L['enabled_label'] = 'Abilita';
-$L['disabled_label'] = 'Disabilita';
